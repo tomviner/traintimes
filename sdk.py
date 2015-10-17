130b4f2388f8d2b6ff7497cd920a08cbaa6e010c
TomV <tom@viner.tv>
2015-10-17 19:31:42 +0100

    tidy sdk

--- sdk.py
+++ sdk.py
@@ -13,47 +13,61 @@ requests_cache.install_cache()
 
 
 class RTTBase(object):
-    base_tpl = 'https://api.rtt.io/api/{version}/{accept}'
+    """ Base Class for RealTimeTrains API
+
+    See http://www.realtimetrains.co.uk/api
+
+    Apply for API access at https://api.rtt.io/
+    Set environ variable:
+        export RTT_AUTH=user:password
+    """
+    # URI template language as per RFC6570
+    base_uri_template = 'https://api.rtt.io/api/{version}/{accept}'
     auth = tuple(os.environ['RTT_AUTH'].split(':'))
 
-    def __init__(self):
-        self.context = {'version': 'v1', 'accept': 'json'}
+    def __init__(self, version='v1', accept='json'):
+        self.context = {'version': version, 'accept': accept}
 
-    @property
-    def url(self):
-        tpl = Template(self.base_tpl + self.tpl)
-        return tpl.expand(self.context).as_string()
-
-    def push(self, key, value):
+    def add_to_context(self, key, value):
         self.context[key] = value
 
+    @property
+    def uri(self):
+        uri_template = Template(self.base_uri_template + self.uri_template)
+        return uri_template.expand(self.context).as_string()
+
     def __repr__(self):
-        return self.url
+        return self.uri
 
     def get(self):
-        return requests.get(self.url, auth=self.auth)
+        return requests.get(self.uri, auth=self.auth)
 
 
 class Location(RTTBase):
-    """Location List API
+    """ Location List API
 
     http://www.realtimetrains.co.uk/api/pull/locationlist
     /(json|xml)/search/<station>/<year>/<month>/<day>/<time>/arrivals
     """
-    tpl = '/search/{station}{+tostation}{+date}{+time}{/arrivals}'
 
-    def __init__(self, station, to_station=None, when=None, arrivals=False):
-        super(Location, self).__init__()
+    uri_template = '/search/{station}{+tostation}{+date}{+time}{/arrivals}'
 
-        self.push('station', station)
+    def __init__(self, station, to_station=None, when=None, arrivals=False, **kwargs):
+        super(Location, self).__init__(**kwargs)
+
+        self.add_to_context('station', station)
+
         if to_station:
-            self.push('tostation', '/to/{}'.format(to_station))
+            self.add_to_context('tostation', '/to/{}'.format(to_station))
+
         if isinstance(when, datetime.date):
-            self.push('date', '/{0:%Y}/{0:%m}/{0:%d}'.format(when))
+            self.add_to_context('date', '/{0:%Y}/{0:%m}/{0:%d}'.format(when))
+
         if isinstance(when, datetime.datetime):
-            self.push('time', '/{0:%H}{0:%M}'.format(when))
+            self.add_to_context('time', '/{0:%H}{0:%M}'.format(when))
+
         if arrivals:
-            self.push('arrivals', 'arrivals')
+            self.add_to_context('arrivals', 'arrivals')
 
 
 class Service(RTTBase):
@@ -62,12 +76,14 @@ class Service(RTTBase):
     http://www.realtimetrains.co.uk/api/pull/serviceinfo
     /(json|xml)/service/<serviceUid>/<year>/<month>/<day>
     """
-    tpl = '/service{/service}{+date}'
 
-    def __init__(self, service, date):
-        super(Service, self).__init__()
+    uri_template = '/service{/service}{+date}'
 
-        self.push('service', service)
+    def __init__(self, service, date, **kwargs):
+        super(Service, self).__init__(**kwargs)
+
+        self.add_to_context('service', service)
+
         assert isinstance(date, datetime.date), \
             "Expected {} got {}".format(datetime.date, type(date))
-        self.push('date', '/{0:%Y}/{0:%m}/{0:%d}'.format(date))
+        self.add_to_context('date', '/{0:%Y}/{0:%m}/{0:%d}'.format(date))
--- test_sdk.py
+++ test_sdk.py
@@ -19,24 +19,24 @@ class TestLocation(object):
     expected_base = 'https://api.rtt.io/api/v1/json/search/'
 
     def test_repr(self):
-        assert Location('HIB').url == repr(Location('HIB'))
+        assert Location('HIB').uri == repr(Location('HIB'))
 
     def test_station(self):
-        assert Location('HIB').url == self.expected_base + 'HIB'
+        assert Location('HIB').uri == self.expected_base + 'HIB'
 
     def test_to_station(self):
-        assert Location('HIB', 'CHX').url == self.expected_base + 'HIB/to/CHX'
+        assert Location('HIB', 'CHX').uri == self.expected_base + 'HIB/to/CHX'
 
     def test_date(self, dt):
-        assert Location('HIB', 'CHX', datetime.date.today()).url == \
+        assert Location('HIB', 'CHX', datetime.date.today()).uri == \
             self.expected_base + 'HIB/to/CHX/2015/10/16'
 
     def test_time(self, dt):
-        assert Location('HIB', 'CHX', datetime.datetime.now()).url == \
+        assert Location('HIB', 'CHX', datetime.datetime.now()).uri == \
             self.expected_base + 'HIB/to/CHX/2015/10/16/2152'
 
     def test_arrivals(self, dt):
-        assert Location('HIB', 'CHX', datetime.datetime.now(), True).url == \
+        assert Location('HIB', 'CHX', datetime.datetime.now(), True).uri == \
             self.expected_base + 'HIB/to/CHX/2015/10/16/2152/arrivals'
 
 
@@ -47,9 +47,9 @@ class TestService(object):
     expected_base = 'https://api.rtt.io/api/v1/json/service/'
 
     def test_service_date(self, dt):
-        assert Service('W12345', datetime.date.today()).url == \
+        assert Service('W12345', datetime.date.today()).uri == \
             self.expected_base + 'W12345/2015/10/16'
 
     def test_service_datetime(self, dt):
-        assert Service('W12345', datetime.datetime.now()).url == \
+        assert Service('W12345', datetime.datetime.now()).uri == \
             self.expected_base + 'W12345/2015/10/16'
