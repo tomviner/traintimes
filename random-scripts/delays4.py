7f0d1a6ccadd324393548191cd3831370cf9beae
Tom Viner <tom@viner.tv>
2016-12-03 01:15:35 +0000

    random scripts

--- random-scripts/delays4.py
+++ random-scripts/delays4.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 # from pprint import pprint
 from collections import Counter
 
@@ -16,7 +16,10 @@ DEST = 'HIB'
 
 
 def calc_lateness(book, real):
-    delta = datetime.strptime(real, '%H%M') - datetime.strptime(book, '%H%M')
+    delta = (
+        datetime.datetime.strptime(real, '%H%M') -
+        datetime.datetime.strptime(book, '%H%M')
+    )
     return int(delta.seconds / 60.0)
 
 def lateness_as_str(loc):
@@ -25,7 +28,7 @@ def lateness_as_str(loc):
     if '?' in (book, real):
         return '{} {}'.format(book, real)
     diff_mins = calc_lateness(book, real)
-    return '{} {:+3.0f}'.format(book, diff_mins)
+    return '{} {:+3.0f} {}'.format(book, diff_mins, real if diff_mins else '\t')
 
 def lateness_as_mins(loc):
     book = loc.get('gbttBookedDeparture', '?')
@@ -36,40 +39,91 @@ def lateness_as_mins(loc):
 
 
 services = []
+import sys
 
-for hour in range(19, 20):
-    date = datetime(2016, 9, 13, hour, 0)
-    print date
+one_day = datetime.timedelta(days=1)
+args = sys.argv[1:]
+today = datetime.date.today()
+
+date = today
+if args:
+    day = args[0]
+    day = int(day)
+    for i in range(31 * 2):
+        date = today - (i * one_day)
+        if date.day == day:
+            break
+    else:
+        raise ValueError(day)
+    day = today.day
+print date
+# sys.exit()
+good_dest = [
+    'HASTING', 'TUNWELL',
+]
+bad_dest = [
+    'HAYS', 'SVNOAKS', 'OREE', 'SLADEGN', 'ORPNGTN', 'FLKSTNC',
+    'GRVSEND', 'RAMSGTE-RAMSGTE', 'TONBDG', 'RAMSGTE', 'SIDCUP', 'DARTFD',
+    'DOVERP', 'BRNHRST', 'GLNGHMK', 'ASHFKY', 'DOVERP-CNTBW', 'CRFD',
+    'RCHT', 'ASHFKY-RAMSGTE', 'NBCKNHM', 'HTHRGRN',
+]
+ds = []
+for hour in range(17, 22):
+    dt = datetime.datetime.combine(date, datetime.time(hour))
+    print dt
     try:
-        data = Location(START, DEST, date).get()
+        data = Location(START, DEST, dt).get()
     except ResponseError as e:
         print(e)
         continue
     if not data['services']:
         continue
+    print len(data['services']), 'trains'
     for s in data['services']:
+
+        das = '-'.join(d['tiploc'] for d in s['locationDetail']['destination'])
+        if das in bad_dest:
+            continue
+        ds.append(das)
         import json
         print json.dumps(s, indent=4)
+        # if '1722' in str(s):
+        #     sys.exit()
         extra = {
             'displayAs': s['locationDetail']['displayAs'],
             'lateness': lateness_as_mins(s['locationDetail']),
         }
         services.append((s['serviceUid'], extra))
-        break
-
+print map(str, Counter(ds).keys())
+# sys.exit()
 displays = []
 for service_uid, extra in services:
     data = Service(service_uid, date).get()
     locations = data['locations']
 
+    locs = [loc['crs'] for loc in locations]
+    das = [loc['displayAs'] for loc in locations]
+    cancelled = 'CANCELLED_CALL' in das or 'TERMINATES' in das
+    # if not cancelled:
+    #     if DEST not in locs:
+    #         continue
+    #     if not extra['lateness']:
+    #         continue
+
+    started = False
     match = False
     output = []
     for loc in locations:
         displays.append(loc['displayAs'])
+        started = started or loc['crs'] in (START, DEST)
+        if not started:
+            continue
         if loc['crs'] == DEST:
+            output.append(' '*30 + 'x' * (lateness_as_mins(loc) or 0))
             match = True
         output.append(loc['crs'] + ' ' + lateness_as_str(loc))
         output[-1] += '\t' + loc['displayAs']
+        # output[-1] += '\t' + loc['gbttBookedDeparture']
         if match:
             break
     print service_uid
