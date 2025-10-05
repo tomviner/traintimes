a9cb40a1e343f0c7f508c517ced40bdf669c3da5
Tom V <tom@viner.tv>
2025-10-05 19:11:12 +0100

    Remove redundant project.yaml metadata

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,12 @@
+"""Pytest configuration helpers."""
+
 import logging
+import os
+
+_PLACEHOLDER_AUTH = "demo:demo"
+
+
+os.environ.setdefault("RTT_AUTH", _PLACEHOLDER_AUTH)
+
 
 logging.basicConfig(level=logging.DEBUG)
