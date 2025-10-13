a14a371c38b26cfa8262eb875654cd11d5db8572
Tom V <tom@viner.tv>
2025-10-13 22:28:41 +0100

    simplify, remove placeholder

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,10 @@
 """Pytest configuration helpers."""
 
 import logging
-import os
 
 from dotenv import load_dotenv
 
 load_dotenv()
-_PLACEHOLDER_AUTH = "demo:demo"
-
-
-os.environ.setdefault("RTT_AUTH", _PLACEHOLDER_AUTH)
 
 
 logging.basicConfig(level=logging.DEBUG)
